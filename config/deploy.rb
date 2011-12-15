46473c3fceecc5a08cd7659bd5145a6157e7d3e2
Antonio Terreno <antonio.terreno@gmail.com>
2011-12-15 10:15:59 +0000

    screw the blue pill, let's use old good way

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,44 +1,78 @@
 require "bundler/capistrano"
 
 set :application, "superupload"
+set :node_file, "app.js"                  
+set :host, "localhost"                    
+set :repository, "git@github.com:aterreno/superuploader.git"
 set :user, "ubuntu"
-set :host, "ec2-107-20-118-43.compute-1.amazonaws.com"
-set :deploy_to, "/var/www/#{application}"
-set :use_sudo, true
+set :admin_runner, 'ubuntu'
+set :application_binary, '/usr/local/bin/node'
 
 set :scm, :git
-set :repository, "git@github.com:aterreno/superuploader.git"
-set :branch, "master"
-set :keep_releases, 3
-
 set :deploy_via, :remote_cache
 role :app, host
-
-set :bluepill, 'bluepill'
-
+set :use_sudo, true
 default_run_options[:pty] = true
 
 namespace :deploy do
   task :start, :roles => :app, :except => { :no_release => true } do
-    run "#{try_sudo :as => 'root'} #{bluepill} start #{application}"
+    run "sudo start #{application}_#{node_env}"
   end
 
   task :stop, :roles => :app, :except => { :no_release => true } do
-    run "#{try_sudo :as => 'root'} #{bluepill} stop #{application}"
+    run "sudo stop #{application}_#{node_env}"
   end
 
   task :restart, :roles => :app, :except => { :no_release => true } do
-    run "#{try_sudo :as => 'root'} #{bluepill} restart #{application}"
+    run "sudo restart #{application}_#{node_env} || sudo start #{application}_#{node_env}"
+  end
+
+  desc "Check required packages and install if packages are not installed"
+  task :update_packages, roles => :app do
+    run "cd #{release_path} && npm install"
   end
 
   task :create_deploy_to_with_sudo, :roles => :app do
-    run "#{try_sudo :as => 'root'} mkdir -p #{deploy_to}"
+    run "sudo mkdir -p #{deploy_to}"
+    run "sudo chown #{admin_runner}:#{admin_runner} #{deploy_to}"
+  end
+  
+  desc "writes the upstart script for running the daemon. Customice to your needs"
+  task :write_upstart_script, :roles => :app do
+    upstart_script = <<-UPSTART
+  description "#{application}"
+
+  start on startup
+  stop on shutdown
+
+  script
+      # We found $HOME is needed. Without it, we ran into problems
+      export HOME="/home/#{admin_runner}"
+      export NODE_ENV="#{node_env}"
+      cd #{current_path}
+
+      exec sudo -u #{admin_runner} sh -c "NODE_ENV=#{node_env} #{application_binary} #{current_path}/#{node_file} #{application_port} >> #{shared_path}/log/#{node_env}.log 2>&1"
+  end script
+  respawn
+UPSTART
+  put upstart_script, "/tmp/#{application}_upstart.conf"
+    run "sudo mv /tmp/#{application}_upstart.conf /etc/init/#{application}_#{node_env}.conf"
   end
 
-  task :npm_install, :roles => :app, :except => { :no_release => true } do
-    run "cd #{release_path} && npm install"
+  desc "Update submodules"
+  task :update_submodules, :roles => :app do
+    run "cd #{release_path}; git submodule init && git submodule update"
   end
+
+  desc "create deployment directory"
+  task :create_deploy_to_with_sudo, :roles => :app do
+    run "sudo mkdir -p #{deploy_to}"
+    run "sudo chown #{admin_runner}:#{admin_runner} #{deploy_to}"
+  end
+
 end
 
+
 before 'deploy:setup', 'deploy:create_deploy_to_with_sudo'
-after 'deploy:finalize_update', 'deploy:npm_install'
+after 'deploy:setup', 'deploy:write_upstart_script'
+after "deploy:finalize_update", "deploy:update_submodules", "deploy:update_packages", 
