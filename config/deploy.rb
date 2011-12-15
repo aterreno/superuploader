824ade71a9a58560ab28474eda1b053068b2d2bc
Antonio Terreno <antonio.terreno@gmail.com>
2011-12-15 10:24:17 +0000

    using an old deploy script that always worked

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,44 +1,51 @@
-require "bundler/capistrano"
+set :application,         "superupload"
+set :node_file,           "app.js"
+set :repository,          "git@github.com:aterreno/superuploader.git"
+set :host,                "ec2-107-20-118-43.compute-1.amazonaws.com"
+set :deploy_to,           "/var/www/#{application}"
+set :repository_cache,    "#{application}_cache"
+set :environment,         "production"
 
-set :application, "superupload"
-set :node_file, "app.js"                  
-set :host, "ec2-107-20-118-43.compute-1.amazonaws.com"                    
-set :repository, "git@github.com:aterreno/superuploader.git"
-set :user, "ubuntu"
-set :admin_runner, 'ubuntu'
-set :application_binary, '/usr/local/bin/node'
-set :deploy_to, "/var/www/#{application}"
+set :deploy_to,           "/var/www/#{application}"
+set :repository_cache,    "#{application}_cache"
+set :environment,         "production"
 
-set :scm, :git
-set :deploy_via, :remote_cache
 role :app, host
-set :use_sudo, true
-default_run_options[:pty] = true
+
+set :branch,              "master"
+set :keep_releases,       3
+set :user,                "ubuntu"
+set :deploy_via,          :remote_cache
+set :scm,                 :git
+set :runner,              "ubuntu"
+
+set :admin_runner,        "ubuntu"
+set :use_sudo,            true
+
+role :app, host,        :primary => true
+
+ssh_options[:forward_agent]   = true
+ssh_options[:paranoid]        = true
+default_run_options[:pty]     = true
 
 namespace :deploy do
   task :start, :roles => :app, :except => { :no_release => true } do
-    run "sudo start #{application}_#{node_env}"
+    run "#{try_sudo :as => 'root'} start #{application}"
   end
 
   task :stop, :roles => :app, :except => { :no_release => true } do
-    run "sudo stop #{application}_#{node_env}"
+    run "#{try_sudo :as => 'root'} stop #{application}"
   end
 
   task :restart, :roles => :app, :except => { :no_release => true } do
-    run "sudo restart #{application}_#{node_env} || sudo start #{application}_#{node_env}"
-  end
-
-  desc "Check required packages and install if packages are not installed"
-  task :update_packages, roles => :app do
-    run "cd #{release_path} && npm install"
+    run "#{try_sudo :as => 'root'} restart #{application} || #{try_sudo :as => 'root'} start #{application}"
   end
 
   task :create_deploy_to_with_sudo, :roles => :app do
-    run "sudo mkdir -p #{deploy_to}"
-    run "sudo chown #{admin_runner}:#{admin_runner} #{deploy_to}"
+    run "#{try_sudo :as => 'root'} mkdir -p #{deploy_to}"
+    run "#{try_sudo :as => 'root'} chown #{admin_runner}:#{admin_runner} #{deploy_to}"
   end
   
-  desc "writes the upstart script for running the daemon. Customice to your needs"
   task :write_upstart_script, :roles => :app do
     upstart_script = <<-UPSTART
   description "#{application}"
@@ -49,31 +56,18 @@ namespace :deploy do
   script
       # We found $HOME is needed. Without it, we ran into problems
       export HOME="/home/#{admin_runner}"
-      export NODE_ENV="#{node_env}"
-      cd #{current_path}
 
-      exec sudo -u #{admin_runner} sh -c "NODE_ENV=#{node_env} #{application_binary} #{current_path}/#{node_file} #{application_port} >> #{shared_path}/log/#{node_env}.log 2>&1"
+      cd #{current_path}
+      exec sudo -u root sh -c "/usr/local/bin/node #{current_path}/#{node_file} production >> #{shared_path}/log/#{application}.log 2>&1"
   end script
   respawn
-UPSTART
+  UPSTART
   put upstart_script, "/tmp/#{application}_upstart.conf"
-    run "sudo mv /tmp/#{application}_upstart.conf /etc/init/#{application}_#{node_env}.conf"
+    run "#{try_sudo :as => 'root'} mv /tmp/#{application}_upstart.conf /etc/init/#{application}.conf"
   end
-
-  desc "Update submodules"
-  task :update_submodules, :roles => :app do
-    run "cd #{release_path}; git submodule init && git submodule update"
-  end
-
-  desc "create deployment directory"
-  task :create_deploy_to_with_sudo, :roles => :app do
-    run "sudo mkdir -p #{deploy_to}"
-    run "sudo chown #{admin_runner}:#{admin_runner} #{deploy_to}"
-  end
-
+  
 end
-
 
 before 'deploy:setup', 'deploy:create_deploy_to_with_sudo'
 after 'deploy:setup', 'deploy:write_upstart_script'
-after "deploy:finalize_update", "deploy:update_submodules", "deploy:update_packages"
+after "deploy:update", "deploy:cleanup" 
